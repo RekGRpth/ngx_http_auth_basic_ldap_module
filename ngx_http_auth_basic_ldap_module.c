877473eea4eb3cf751047f9b85c868c06ea5b03c
RekGRpth <rekgrpth@gmail.com>
2022-02-17 10:00:59 +0500

    rename

--- ngx_http_auth_basic_ldap_module.c
+++ ngx_http_auth_basic_ldap_module.c
@@ -65,8 +65,8 @@ static char *ngx_http_auth_basic_ldap_attr_conf(ngx_conf_t *cf, ngx_command_t *c
 static char *ngx_http_set_complex_value_slot_enable(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
     ngx_http_auth_basic_ldap_loc_conf_t *lcf = conf;
     if (!lcf->bind) return "!bind";
-    ngx_http_auth_basic_ldap_main_conf_t *main = ngx_http_conf_get_module_main_conf(cf, ngx_http_auth_basic_ldap_module);
-    main->enable = 1;
+    ngx_http_auth_basic_ldap_main_conf_t *mcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_auth_basic_ldap_module);
+    mcf->enable = 1;
     return ngx_http_set_complex_value_slot(cf, cmd, conf);
 }
 
@@ -329,8 +329,8 @@ static ngx_int_t ngx_http_auth_basic_ldap_handler(ngx_http_request_t *r) {
 }
 
 static ngx_int_t ngx_http_auth_basic_ldap_postconfiguration(ngx_conf_t *cf) {
-    ngx_http_auth_basic_ldap_main_conf_t *main = ngx_http_conf_get_module_main_conf(cf, ngx_http_auth_basic_ldap_module);
-    if (!main->enable) return NGX_OK;
+    ngx_http_auth_basic_ldap_main_conf_t *mcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_auth_basic_ldap_module);
+    if (!mcf->enable) return NGX_OK;
     ngx_http_core_main_conf_t *core_main_conf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
     ngx_http_handler_pt *handler = ngx_array_push(&core_main_conf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
     if (!handler) return NGX_ERROR;
@@ -339,9 +339,9 @@ static ngx_int_t ngx_http_auth_basic_ldap_postconfiguration(ngx_conf_t *cf) {
 }
 
 static void *ngx_http_auth_basic_ldap_create_main_conf(ngx_conf_t *cf) {
-    ngx_http_auth_basic_ldap_main_conf_t *main = ngx_pcalloc(cf->pool, sizeof(*main));
-    if (!main) return NULL;
-    return main;
+    ngx_http_auth_basic_ldap_main_conf_t *mcf = ngx_pcalloc(cf->pool, sizeof(*mcf));
+    if (!mcf) return NULL;
+    return mcf;
 }
 
 static void *ngx_http_auth_basic_ldap_create_loc_conf(ngx_conf_t *cf) {
